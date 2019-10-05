842d8c687ce3427d3053ddbbd246def378e25847
Edward Eisenberger <eeisenberger3@gmail.com>
2019-10-04 21:09:30 -0400

    Completed assignment 05

--- Assignment05/HammingCodeCSharp/Program.cs
+++ Assignment05/HammingCodeCSharp/Program.cs
@@ -25,10 +25,7 @@
                   {
                      int kiCode = MHam( koArgs[ 1 ][ 0 ] );
                      Console.WriteLine( "Code: " );
-                     for( int i = 10; i >= 0; i-- )
-                     {
-                        Console.Write( ( kiCode & ( 0x00000001 << i ) ) >> i );
-                     }
+                     MPrintCode( kiCode, 11 );
                      Console.WriteLine( );
                      break;
                   }
@@ -38,6 +35,13 @@
                      int kiCode = Convert.ToInt32( koArgs[ 1 ], 2 );
                      char kcChar = MDeham( kiCode );
                      Console.WriteLine( "Character: " + kcChar );
+                     break;
+                  }
+                  case "b":
+                  case "burst":
+                  {
+                     string koOut = MBurst( koLine.Substring( koLine.IndexOf( " " ) + 1 ) );
+                     Console.WriteLine( "Received String: " + koOut );
                      break;
                   }
                   default:
@@ -60,6 +64,15 @@
          Console.WriteLine( "   h <c>        Outputs the hamming code for given character, c" );
          Console.WriteLine( "   d <c>        Outputs the character for the given hamming code, c" );
          Console.WriteLine( "   b <s>        Input string, s, is hammed, then de-hammed and printed" );
+      }
+
+      static void MPrintCode( int aiCode, int aiBits )
+      {
+         Console.Write( "   " );
+         for( int i = ( aiBits - 1 ); i >= 0; i-- )
+         {
+            Console.Write( ( aiCode & ( 0x00000001 << i ) ) >> i );
+         }
       }
 
       static int MHam( char acChar )
@@ -123,15 +136,77 @@
          }
 
          // If valid
-         if( ( kiCount > 0 ) && ( kiCount < 12 ) )
+         if( ( kiCount > 0 ) && ( kiCount < 11 ) )
          {
-            kiBit[ kiCount ] = ( kiBit[ kiCount ] == 0 ) ? 1 : 0;
+            kiBit[ 11 - kiCount ] = ( kiBit[ 11 - kiCount ] == 0 ) ? 1 : 0;
          }
 
          kcChar = ( char )( ( kiBit[ 0 ] << 0 ) | ( kiBit[ 1 ] << 1 ) | ( kiBit[ 2 ] << 2 ) | ( kiBit[ 4 ] << 3 ) |
                             ( kiBit[ 5 ] << 4 ) | ( kiBit[ 6 ] << 5 ) | ( kiBit[ 8 ] << 6 ) );
 
          return( kcChar );
+      }
+
+      static string MBurst( string aoStr )
+      {
+         string koOut;
+
+         int    kiCode;
+         int[ ] kiHam = new int[ aoStr.Length ];
+         int    kiMsg;     // One burst message, supports up to 32 character bits
+         int[ ] kiRcv = new int[ aoStr.Length ];
+
+         Array.Clear( kiHam, 0, kiRcv.Length );
+         Array.Clear( kiRcv, 0, kiRcv.Length );
+
+         // Get hamming code for each character
+         Console.WriteLine( "Hamming Code Matrix:" );
+         for( int i = 0; i < aoStr.Length; i++ )
+         {
+            kiHam[ i ] = MHam( aoStr[ i ] );
+            MPrintCode( kiHam[ i ], 11 );
+            Console.WriteLine( );
+         }
+
+         // Transmit one column at a time
+         Console.WriteLine( "Transmitting Messages: " );
+         for( int kiBit = 0; kiBit < 11; kiBit++ )
+         {
+            // Build Transmit Message
+            kiMsg = 0;
+            for( int i = 0; i < kiHam.Length; i++ )
+            {
+               kiCode = ( kiHam[ i ] & ( 1 << kiBit ) ) >> kiBit;
+               kiMsg |= kiCode << i;
+            }
+
+            Console.Write( "   Bit {0,2}: ", kiBit );
+            MPrintCode( kiMsg, kiHam.Length );
+            Console.WriteLine( );
+
+            // Receive Transmit Message
+            for( int i = 0; i < kiRcv.Length; i++ )
+            {
+               kiCode = ( kiMsg & ( 1 << i ) ) >> i;
+               kiRcv[ i ] |= kiCode << kiBit;
+            }
+         }
+
+         Console.WriteLine( "Received Hamming Code Matrix:" );
+         for( int i = 0; i < aoStr.Length; i++ )
+         {
+            MPrintCode( kiRcv[ i ], 11 );
+            Console.WriteLine( );
+         }
+
+         // Build received message
+         koOut = "";
+         for( int i = 0; i < kiRcv.Length; i++ )
+         {
+            koOut += MDeham( kiRcv[ i ] );
+         }
+
+         return( koOut );
       }
    }
 }
